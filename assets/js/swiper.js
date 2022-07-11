db907a98e2f489aa97fd33d2e7533f6cbc7347a9
Edgar Perez <edgarjoel9912@gmail.com>
2022-07-11 14:03:11 -0400

    Aplique swiper js en la pagina de infoViaje

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,16 +1,13 @@
-/* let swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 28000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }
-    // navigation: {
-    //   nextEl: ".swiper-button-next",
-    //   prevEl: ".swiper-button-prev",
-    // },
-  }); */
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: 'bullets',
+    clickable: true
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true
+  }
+});
