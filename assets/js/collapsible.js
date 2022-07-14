625d2c6d238f323fe8ec6576068df630cda2ddd5
Edgar Perez <edgarjoel9912@gmail.com>
2022-07-14 16:59:15 -0400

    Actualice la pagina de infoViaje al diseño actual en figma, agregue los nuevos iconos y cree la logica y diseño para el toggle de ver mas

---
+++ assets/js/collapsible.js
@@ -0,0 +1,14 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var places_list = this.nextElementSibling;
+    if (places_list.style.display === "flex") {
+      places_list.style.display = "none";
+    } else {
+      places_list.style.display = "flex";
+    }
+  });
+}
