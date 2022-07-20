5024a660e7851296e1cf14a063b478f17abf3f35
Edgar Perez <edgarjoel9912@gmail.com>
2022-07-20 19:55:00 -0400

    Realice la estructura, los estilos y la logica para el boton dropdown del menu

--- assets/js/menuMobile.js
+++ assets/js/menuMobile.js
@@ -1,19 +1,51 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight,
+//               sectionTop = current.offsetTop - 58,
+//               sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.prueba');
+    const menu_dropdown = dropdown.querySelector('.menu_dropdown');
+    const options = dropdown.querySelector('.menu_dropdown li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('prueba-rotate');
+        menu_dropdown.classList.toggle('menu_dropdown-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select-clicked');
+            caret.classList.remove('prueba-rotate');
+            menu_dropdown.classList.remove('menu_dropdown-open');
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+
+            option.classList.add('active');
+        });
+    });
+});
