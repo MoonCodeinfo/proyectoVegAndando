5a13d53a2d3f04a69b5b84af8137e0b4a110b2dd
Edgar Perez <edgarjoel9912@gmail.com>
2022-07-21 18:44:11 -0400

    Actualice el diseño del menu dropdown y agruegue el archivo gitignore

--- assets/js/menuMobile.js
+++ assets/js/menuMobile.js
@@ -24,14 +24,12 @@ const dropdowns = document.querySelectorAll('.dropdown');
 dropdowns.forEach(dropdown => {
 
     const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.prueba');
     const menu_dropdown = dropdown.querySelector('.menu_dropdown');
     const options = dropdown.querySelector('.menu_dropdown li');
     const selected = dropdown.querySelector('.selected');
 
     select.addEventListener('click', () => {
         select.classList.toggle('select-clicked');
-        caret.classList.toggle('prueba-rotate');
         menu_dropdown.classList.toggle('menu_dropdown-open');
     });
 
@@ -39,7 +37,6 @@ dropdowns.forEach(dropdown => {
         option.addEventListener('click', () => {
             selected.innerText = option.innerText;
             select.classList.remove('select-clicked');
-            caret.classList.remove('prueba-rotate');
             menu_dropdown.classList.remove('menu_dropdown-open');
             options.forEach(option => {
                 option.classList.remove('active');
