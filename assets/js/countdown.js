a358c03320c20434fa803eca76ad547147ffce21
Edgar Perez <edgarjoel9912@gmail.com>
2022-07-26 21:29:43 -0400

    infoViaje actualizada al diseño tablet

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("July 24, 2022 00:00:00").getTime();
+    const countDate = new Date("July 31, 2022 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
