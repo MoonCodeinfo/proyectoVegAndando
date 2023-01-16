b7ecbc931c12b601dd30e79b7ecd803f72dc7fee
Edgar Perez <edgarjoel9912@gmail.com>
2023-01-16 15:21:14 -0400

    Moca travel added up

--- assets/viajes/moca/countdown.js
+++ assets/viajes/moca/countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("November 05, 2022 00:00:00").getTime();
+    const countDate = new Date("march 12, 2023 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
