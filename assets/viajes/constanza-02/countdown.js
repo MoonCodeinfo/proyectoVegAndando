ea70da5d187744f64e5ea038feb4aa34efd2b6f4
Edgar Perez <edgarjoel9912@gmail.com>
2023-01-27 13:10:07 -0400

    Updates en al css, viajes actualizados y viaje nuevo creado

--- assets/viajes/constanza-02/countdown.js
+++ assets/viajes/constanza-02/countdown.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const countDate = new Date("January 28, 2023 00:00:00").getTime();
+  const countDate = new Date("April 28, 2023 00:00:00").getTime();
   const now = new Date().getTime();
   const gap = countDate - now;
 
